bec5ee13c59791d3f31e11c9ea4f3500f33ad1ab
Marcin Rzepa <marcin.rzepa@ericsson.com>
2018-11-01 14:28:17 +0100

    Poprawka Y - hotfix branch

--- test.rb
+++ test.rb
@@ -1,2 +1,2 @@
-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX common base
+YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY common base
 ten plik (o tej samej nazwie) co na branch testing - tylko stworzony na master.
